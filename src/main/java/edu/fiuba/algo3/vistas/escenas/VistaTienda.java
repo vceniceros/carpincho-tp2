3eb60faff432096fb05ecd874d3aa918be5357b5
axeel25 <abillordo@fi.uba.ar>
2024-12-02 20:51:46 -0300

    update Vista tienda

--- src/main/java/edu/fiuba/algo3/vistas/escenas/VistaTienda.java
+++ src/main/java/edu/fiuba/algo3/vistas/escenas/VistaTienda.java
@@ -64,17 +64,25 @@ public class VistaTienda extends Scene{
 
         FlowPane contenedorCartas = new FlowPane();
         contenedorCartas.setOrientation(Orientation.HORIZONTAL);
-        contenedorCartas.setPrefHeight(150);  // Establecer altura fija para el contenedor
+        contenedorCartas.setPrefHeight(180);  // Establecer altura fija para el contenedor
 
 
         FlowPane contenedorTarots = new FlowPane();
         contenedorTarots.setOrientation(Orientation.HORIZONTAL);
-        contenedorTarots.setPrefHeight(150);  // Establecer altura fija para el contenedor
+        contenedorTarots.setPrefHeight(180);  // Establecer altura fija para el contenedor
 
 
         FlowPane contenedorComodines = new FlowPane();
         contenedorComodines.setOrientation(Orientation.HORIZONTAL);
         contenedorComodines.setPrefHeight(150);  // Establecer altura fija para el contenedor
+
+        // Aca empiezo a tocar
+        VBox panelDetalles = new VBox();
+        panelDetalles.setAlignment(Pos.CENTER);
+        panelDetalles.setSpacing(10);
+        panelDetalles.setStyle("-fx-background-color: rgba(0, 0, 0, 0.8); -fx-padding: 20px; -fx-border-color: white; -fx-border-width: 2px;");
+        panelDetalles.setVisible(false); // Ocultarlo inicialmente
+        root.getChildren().add(panelDetalles);
 
         for (Carta carta : cartas) {
             Button botonCarta = new Button();
@@ -83,7 +91,20 @@ public class VistaTienda extends Scene{
             ImageView imagenCarta = (ImageView) botonCarta.getGraphic();
             imagenCarta.setFitWidth(80);
             imagenCarta.setFitHeight(120);
-            botonCarta.setOnAction(event -> mostrarDetallesCarta(carta));
+
+            botonCarta.setOnMouseEntered(event -> {
+                imagenCarta.setFitWidth(100);
+                imagenCarta.setFitHeight(140);
+            });
+            botonCarta.setOnMouseExited(event -> {
+                imagenCarta.setFitWidth(80);
+                imagenCarta.setFitHeight(120);
+            });
+
+            // Mostrar detalles de la carta al hacer click
+            botonCarta.setOnMouseClicked(event -> {
+                mostrarDetallesCarta(carta, panelDetalles, event.getSceneX(), event.getSceneY());
+            });
             contenedorCartas.getChildren().add(botonCarta);
         }
 
@@ -94,7 +115,15 @@ public class VistaTienda extends Scene{
             ImageView imagenCarta = (ImageView) botonTarot.getGraphic();
             imagenCarta.setFitWidth(80);
             imagenCarta.setFitHeight(120);
-            botonTarot.setOnAction(event -> mostrarDetallesCarta(tarot));
+            botonTarot.setOnMouseEntered(event -> {
+                imagenCarta.setFitWidth(100);
+                imagenCarta.setFitHeight(140);
+            });
+            botonTarot.setOnMouseExited(event -> {
+                imagenCarta.setFitWidth(80);
+                imagenCarta.setFitHeight(120);
+            });
+            botonTarot.setOnMouseClicked(event -> mostrarDetallesCarta(tarot, panelDetalles, event.getSceneX(),event.getSceneY()));
             contenedorTarots.getChildren().add(botonTarot);
         }
 
@@ -105,16 +134,31 @@ public class VistaTienda extends Scene{
             ImageView imagenCarta = (ImageView) botonComodin.getGraphic();
             imagenCarta.setFitWidth(80);
             imagenCarta.setFitHeight(120);
-            botonComodin.setOnAction(event -> mostrarDetallesCarta(comodin));
+            botonComodin.setOnMouseEntered(event -> {
+                imagenCarta.setFitWidth(100);
+                imagenCarta.setFitHeight(140);
+            });
+            botonComodin.setOnMouseExited(event -> {
+                imagenCarta.setFitWidth(80);
+                imagenCarta.setFitHeight(120);
+            });
+            botonComodin.setOnMouseClicked(event -> mostrarDetallesCarta(comodin, panelDetalles, event.getX(), event.getY()));
             contenedorComodines.getChildren().add(botonComodin);
         }
 
         VBox contenedorCartasTarotsComodines = new VBox();
         contenedorCartasTarotsComodines.setAlignment(Pos.TOP_CENTER);
+
+        Label textoCartaNormal = new Label("Normales: ");
+        textoCartaNormal.setStyle("-fx-background-color: violet; -fx-text-fill: black; -fx-padding: 5px; -fx-border-color: black; -fx-border-width: 1px;");
+        Label textoCartaTarot = new Label(" Tarots: ");
+        textoCartaTarot.setStyle("-fx-background-color: violet; -fx-text-fill: black; -fx-padding: 5px; -fx-border-color: black; -fx-border-width: 1px;");
+        Label textoComodin = new Label("Comodines: ");
+        textoComodin.setStyle("-fx-background-color: violet; -fx-text-fill: black; -fx-padding: 5px; -fx-border-color: black; -fx-border-width: 1px;");
         contenedorCartasTarotsComodines.getChildren().addAll(
-                new Label("Cartas Normales"), contenedorCartas,
-                new Label("Tarots"), contenedorTarots,
-                new Label("Comodines"), contenedorComodines
+                textoCartaNormal, contenedorCartas,
+                textoCartaTarot, contenedorTarots,
+                textoComodin, contenedorComodines
         );
 
         VBox contenedorPrincipal = new VBox(20);
@@ -153,14 +197,38 @@ public class VistaTienda extends Scene{
         return "Palo desconocido";
     }
 
-    private void mostrarDetallesCarta(Object carta) {
-        String detalles = "Nombre: " + carta.toString() + "\nDescripción: " + obtenerDescripcionCarta(carta);
-        System.out.println(detalles);
+    private void mostrarDetallesCarta(Object carta, VBox panelDetalles, double x, double y) {
+        panelDetalles.getChildren().clear();
+        panelDetalles.setVisible(true);
+        panelDetalles.setStyle("-fx-background-color: rgba(1, 1, 1, 0.8); -fx-border-color: white; -fx-border-width: 2; -fx-padding: 20;");
+
+        // Dimensiones y posición del panel
+        panelDetalles.setPrefWidth(375);
+        panelDetalles.setPrefHeight(250);
+        //panelDetalles.setLayoutX(stage.getWidth() - 320);  // Posición fija al costado derecho
+        //panelDetalles.setLayoutY((stage.getHeight() - 200) / 2);  // Centrado verticalmente
+
+        panelDetalles.setLayoutX((stage.getWidth() - panelDetalles.getPrefWidth()) / 2);  // Centrado horizontalmente
+        panelDetalles.setLayoutY((stage.getHeight() - panelDetalles.getPrefHeight()) / 2);  // Centrado verticalmente
+        panelDetalles.setAlignment(Pos.CENTER);
+
+        Label titulo = new Label("Detalles de la carta:");
+        titulo.setStyle("-fx-font-weight: bold; -fx-text-fill: white; -fx-font-size: 25px;");
+        titulo.setLayoutY(10);
+
+        Label descripcion = new Label(obtenerDescripcionCarta(carta));
+        descripcion.setWrapText(true);
+        descripcion.setPrefWidth(260);
+        descripcion.setStyle("-fx-font-weight: bold; -fx-text-fill: white; -fx-font-size: 16px;");
+
+        panelDetalles.getChildren().addAll(titulo, descripcion);
+        //String detalles = "Nombre: " + carta.toString() + "\nDescripción: " + obtenerDescripcionCarta(carta);
+        //System.out.println(detalles);
     }
 
     private String obtenerDescripcionCarta(Object carta) {
         if (carta instanceof Carta) {
-            return "";
+            return "No tiene descripción";
         } else if (carta instanceof Tarot) {
             return "Descripción del tarot: " + ((Tarot) carta).getDescripcion();
         } else if (carta instanceof Comodin) {
